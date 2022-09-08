697b79dd033413e44dbe5458dc20486be8dc2cf5
jkbehling <90301783+jkbehling@users.noreply.github.com>
2022-09-08 13:53:17 -0600

    Got the readMemberList function to work

--- TeamGenerator.go
+++ TeamGenerator.go
@@ -1,12 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	
+	fmt.Println("Hello")
 }
 
-func readMemberList() {}
+func readMemberList() []string {
+	//open student_names file
+	file, err := os.Open("student_names.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	//Create Slice
+	var students []string
+	//Add student names from text file to slice
+	for scanner.Scan() {
+		students = append(students, scanner.Text())
+	}
+
+	return students
+}
 
 func randomizeAndAssign() {}
 
