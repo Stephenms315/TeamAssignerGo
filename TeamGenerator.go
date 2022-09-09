a158c8e34565bac7bd22ce3551fba399ef8a9207
Stephenms315 <78623118+Stephenms315@users.noreply.github.com>
2022-09-09 09:26:03 -0600

    Update TeamGenerator.go

--- TeamGenerator.go
+++ TeamGenerator.go
@@ -8,7 +8,27 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello")
+	//----------Inputs---------
+	names := [5]string{"a", "b", "d", "e", "f"}
+	var grpSiz int = 2
+
+	fmt.Println(names)
+	fmt.Println(grpSiz)
+	fmt.Println("-------")
+	//----------Vars-----------
+	var numStu int = len(names)
+	var numGrp int = (numStu / grpSiz)
+	var grpRm int = numStu - (grpSiz * numGrp)
+
+	fmt.Println(numStu)
+	fmt.Println(numGrp)
+	fmt.Println(grpSiz)
+	fmt.Println(grpRm)
+	fmt.Println("-------")
+	//----------Spots----------
+	//----------Assigner-------
+
+	
 }
 
 func readMemberList() []string {
@@ -31,6 +51,8 @@ func readMemberList() []string {
 	return students
 }
 
-func randomizeAndAssign() {}
+func randomizeAndAssign() {
+
+}
 
 func printTeams() {}
